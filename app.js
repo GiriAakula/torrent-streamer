e86b946e66f5e1806d622d66436551082d63103e
giriaakula <giriakula3@gmail.com>
2022-01-01 15:30:19 +0530

    seedr credentials added

--- app.js
+++ app.js
@@ -1,68 +1,99 @@
 var Seedr = require("seedr");
 var seedr = new Seedr();
-const Downloader = require('nodejs-file-downloader');
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const path = require('path');
-let sendStatus;
-let movieName = '';
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+var db = new JsonDB(new Config("user_database", true, false, '/'));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/sendMagnet", function(req, res){
-    console.log(req.query)
-    if(req.query.magnet){
-        sendStatus(`${req.query.magnet} has been received`)
-        main(req.query.magnet)
+
+app.post("/newUser", function (req, res) {
+    if (req.body.username && req.body.password && req.body.magnet) {
         res.sendStatus(200)
-    }else {
-        res.send({error: "Magnet link is required"})
-        sendStatus(`A proper magnet link is required.`)
+        main(req.body.magnet, req.body.username, req.body.password)
     }
 });
 
-app.get('/status', function(req, res){
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
-    })
-    sendStatus = function(data){
-        res.write("data: " + data + "\n\n")
+app.get("/videoUrl", function (req, res) {
+    let username;
+    if (req.query.username === "me") {
+        username = "giriakula3@gmail.com";
+    } else {
+        username = req.query.username;
+    }
+    console.log('getting video url')
+    if (username) {
+        try {
+            let timestamp = db.getData(`/users/${username}/timestamp`);
+            let timeDiff = Math.abs(new Date().getTime() - timestamp) / 1000;
+            console.log(timeDiff)
+            if(timeDiff > 30){
+                res.json({timeout:true})
+                return;
+            }
+            let users = db.getData(`/users/${username}/videoUrl`);
+            if (users) {
+                res.send(users)
+            } else {
+                res.sendStatus(204)
+            }
+        } catch (error) {
+            res.sendStatus(204)
+        }
     }
 })
 app.listen(process.env.PORT || 8000, function () {
     console.log("Listening on port 8000!");
 });
 
-async function main(magnet) {
+async function main(magnet, usrname, pasword) {
     try {
-        await seedr.login("giriakula3@gmail.com", "9701732638");
-        sendStatus("Logging into your seedr account...")
+        let username = "";
+        let password = "";
+        if (usrname === "me" && pasword === "me") {
+            username = "giriakula3@gmail.com";
+            password = "9701732638"
+        } else {
+            username = usrname;
+            password = pasword;
+        }
+        db.push(`/users/${username}`, { timestamp: Date.now() }, false);
+        try {
+            if(db.getData(`/users/${username}/videoUrl`)){
+                db.delete(`/users/${username}/videoUrl`);
+            }
+        } catch (error) {
+           console.log("New user. No videoUrl found")
+        }
+        console.log(username, password)
+        await seedr.login(username, password);
+        console.log('logged in')
         var contents = [];
         const videos = await seedr.getVideos();
-        if(videos?.flat()[0]?.fid){
+        if (videos?.flat()[0]?.fid) {
             await seedr.deleteFolder(videos.flat()[0].fid);
-            sendStatus("Deleting old videos in your seedr account...")
         }
-        async function addMagnet(){
+        async function addMagnet() {
+            console.log('adding magnet')
             await seedr.addMagnet(magnet);
             contents = await seedr.getVideos()
-            if(contents.length > 0){
-                sendStatus(`Adding magnet link...`)
+            if (contents.length > 0) {
                 getDownloadUrl()
-            }else {
+            } else {
                 addMagnet()
             }
         };
         addMagnet();
-        async function getDownloadUrl(){
+        async function getDownloadUrl() {
             let url = await seedr.getFile(contents.flat()[0].id);
-            console.log(url.url, "from here")
-            sendStatus(`Done|||${url.url}`)
+            console.log(url, 'url')
+            db.push(`/users/${username}`, { videoUrl: url }, false);
         }
     } catch (error) {
         console.log(error, 'error')
